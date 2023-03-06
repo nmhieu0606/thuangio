611986c056cc3c2082eb21598e19c90de1a15a10
nmhieu0606 <hieuggoag1234a@gmail.com>
2023-03-06 19:56:31 +0700

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-// import image from "@astrojs/image";
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import netlify from '@astrojs/netlify/functions';
