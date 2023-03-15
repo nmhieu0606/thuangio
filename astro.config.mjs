b793060b58c60a38c569b7e44ada431dda99694b
nmhieu0606 <hieuggoag1234a@gmail.com>
2023-03-15 16:06:03 +0700

    1

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind"; // https://astro.build/config
+import markdownIntegration from '@astropub/md'
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  integrations: [tailwind(),markdownIntegration()],
+  adapter: netlify(),
+  markdown: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // syntaxHighlight: 'shiki'
+    // syntaxHighlight: 'prism'
+  }
+});
